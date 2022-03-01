392b115e4e33d64b488fcd28f1a516d713710129
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-03-01 19:04:28 +0000

    Final style on card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,13 +10,13 @@ export default function Card(props){
             <div className="card--text">    
             <div className="card--stats">
                 <img src={require(`../images/icon.png`)} alt="" />
-                <p className="card-country">{props.item.location}</p>
+                <p className="card--country">{props.item.location}</p>
                 <p className="card--link">View on Google Maps</p>
             </div>
             <div className="card--content">
                 <h2 className="card--title">{props.item.title}</h2>
                 <p className="card--date">{props.item.date}</p>
-                <p className="card--date">{props.item.description}</p>
+                <p className="card--description">{props.item.description}</p>
             </div>
             </div>
         </div>
